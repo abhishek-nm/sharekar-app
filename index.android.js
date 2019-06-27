90643be1d4dda526f6dad94a329687185c21f7e9
kumar-abhishek <kumar.abhishek@247-inc.com>
2019-06-27 13:03:24 +0530

    changed config for running in local

---
+++ index.android.js
@@ -0,0 +1,11 @@
+/**
+* This is the start point of the app on the Android platform
+* You can ignore this file since it only links to the src/Main.js file
+**/
+
+import { AppRegistry } from 'react-native';
+import Main from './src/Main';
+
+console.disableYellowBox = true;
+
+AppRegistry.registerComponent('Shop', () => Main);
